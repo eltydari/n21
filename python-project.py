41b92b7436ae997d09906c6b3c80ae4e737a4213
eltydari <2453869+eltydari@users.noreply.github.com>
2020-09-23 00:02:00 -0400

    initial commit - adding the required files

---
+++ python-project.py
@@ -0,0 +1,21 @@
+class Tokenizer(object):
+    def __init__(self, path):
+        pass
+
+    def nextToken(self):
+        pass
+
+
+class TrieParser(object):
+    def __init__(self, path):
+        pass
+
+    def _make_trie(self, str_in):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
