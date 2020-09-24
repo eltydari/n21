188c2977046be1fc2572a9efa081363e97aad44e
eltydari <2453869+eltydari@users.noreply.github.com>
2020-09-23 20:26:25 -0400

    Adding argparse

--- python-project.py
+++ python-project.py
@@ -1,3 +1,7 @@
+import argparse
+import os.path
+
+
 class Tokenizer(object):
     def __init__(self, path):
         pass
@@ -15,7 +19,18 @@ class TrieParser(object):
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='Process a file looking for specific words/phrases.')
+    parser.add_argument('documentPath', type=str, help='Path to the document to be processed.')
+    parser.add_argument('--configPath', default=os.path.join(".", "config.txt"),
+                        help='Path to the configuration file containg words/phrases.')
+
+    args = parser.parse_args()
+    with open(args.documentPath) as f:
+        print(f.read())
+    print(args.documentPath)
+    with open(args.configPath) as f:
+        print(f.read())
+    print(args.configPath)
 
 if __name__ == "__main__":
     main()
